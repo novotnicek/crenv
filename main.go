e586e502c24b9e83aa3661069cb1623032d8a957
Peter Novotnicek <peter.novotnicek@core4.sk>
2024-07-23 16:40:15 +0200

    add quotes for values, update crenv binary

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ func main() {
 		}
 
 		fmt.Printf("Key: %s, Value: %s\n", key, env)
-		fmt.Fprintf(file, "%s=%s\n", key, env)
+		fmt.Fprintf(file, "%s=\"%s\"\n", key, env)
 	}
 
 	fmt.Println("====================================")
